e52229a4e4a21ad4a97fb81b768f560533dbee3c
최승연 <choiseungyeon416@gmail.com>
2022-02-15 14:15:30 +0900

    view 완성

--- app/src/main/java/com/example/androidhttplibrarytest/MainActivity.kt
+++ app/src/main/java/com/example/androidhttplibrarytest/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.androidhttplibrarytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.example.androidhttplibrarytest.databinding.ActivityMainBinding
+import com.example.androidhttplibrarytest.retrofit.PapagoRes
+import com.example.androidhttplibrarytest.retrofit.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, resources.getStringArray(R.array.spinner_target))
     }
 }
